b6d459f5452d595b0f22cdad3a1eb867e18168aa
KuoPing Kao <igaozp@hotmail.com>
2017-11-24 19:00:08 +0800

    [Add] Application.java

---
+++ src/main/java/cambrian/Application.java
@@ -0,0 +1,16 @@
+package cambrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author igaozp
+ * @version 1.0
+ * @since 2017-11-23
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
