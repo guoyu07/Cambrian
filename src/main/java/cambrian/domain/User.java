de5422cdc470b9a138e64bceb94ca3942152060a
KuoPing Kao <igaozp@hotmail.com>
2017-12-07 17:31:27 +0800

    [Add] 添加用户的实体类 User.java

---
+++ src/main/java/cambrian/domain/User.java
@@ -0,0 +1,66 @@
+package cambrian.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author igaozp
+ * @version 1.0
+ * @since 2017-12-06
+ */
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private Permission permission;
+    private Date createTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
